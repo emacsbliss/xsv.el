a35bd94e25a14c93a722968ee1a219ca31a9010c
emacsbliss <emacsbliss@gmail.com>
2021-04-06 22:59:34 -0400

    support select command

--- xsv.el
+++ xsv.el
@@ -36,7 +36,7 @@ internal format."
       (error "Wrong input, should be column-name ='regex' or column-name != 'regex'"))
 ))
 
-(defun xsv/build-command (filter)
+(defun xsv/build-search-command (filter)
   "`FILTER' is a list of three elements in the order of column, operator, regex.
 Return the xsv command for the given `FILTER'."
     (let ((column (nth 0 filter))
@@ -48,6 +48,50 @@ Return the xsv command for the given `FILTER'."
           (setq invert "-v"))
 
         (format "xsv search %s -s %s %s" invert column regex)))
+
+(defun xsv/run (input cmd-builder)
+  "Invoke xsv to run the `COMMAND' then collect output and show it in buffer."
+  (let* ((csv-file (buffer-file-name))
+         (content)
+         (buf)
+         (command)
+         (result))
+
+   ;; if cur-file is nil, ie we may visit a buffer which is not a file
+   ;; then we need to save buffer content to a temp file first
+   (unless csv-file
+     (setq content (buffer-substring (point-min) (point-max)))
+     (setq csv-file (make-temp-file "xsv-"))
+     (with-temp-file csv-file
+       (insert content)))
+
+   (setq command (funcall cmd-builder input csv-file))
+   (message "command: %s" command)
+   (setq buf (get-buffer-create (generate-new-buffer-name "*xsv*")))
+   (set-buffer buf)
+   (setq result (shell-command-to-string command))
+   (insert result)
+   (select-window (split-window-below))
+   (switch-to-buffer buf)
+))
+
+(defun xsv/search-cmd-builder (input csv-file)
+  "Build the actual xsv command from given `INPUT' and `CSV-FILE'."
+  (let* ((filters (xsv/split-filters input))
+         (parts (mapcar #'xsv/parse-filter filters))
+         (commands (mapcar #'xsv/build-search-command parts))
+         (cmd))
+
+   (setq cmd
+         (s-join " | " (cons (s-concat (car commands) " " csv-file)
+                     (-drop 1 commands))))
+
+   cmd
+))
+
+(defun xsv/select-cmd-builder (input csv-file)
+  "Build the actual xsv select command from given `INPUT' and `CSV-FILE'."
+  (format "xsv select %s %s" input csv-file))
 
 (defun xsv/filter (input)
   "For the given `INPUT', run the xsv search on the current buffer.
@@ -67,34 +111,20 @@ columnC NOT matches regexC."
   (interactive
    (list (read-string "input: ")))
 
-  (let* ((filters (xsv/split-filters input))
-         (parts (mapcar #'xsv/parse-filter filters))
-         (commands (mapcar #'xsv/build-command parts))
-         (cur-file (buffer-file-name))
-         (content)
-         (final-cmd)
-         (buf)
-         (result))
+  (xsv/run input #'xsv/search-cmd-builder))
 
-   ;; if cur-file is nil, ie we may visit a buffer which is not a file
-   ;; then we need to save buffer content to a temp file first
-   (unless cur-file
-     (setq content (buffer-substring (point-min) (point-max)))
-     (setq cur-file (make-temp-file "xsv-"))
-     (with-temp-file cur-file
-       (insert content)))
+(defun xsv/select (columns)
+  "For the given `COLUMNS', run the xsv select on the current buffer.
+Save the search result into a new buffer with prefix `xsv-'.
+`COLUMNS' should contain columns separated with comma and no space in between.
+For eg: 'Transaction Type',Date
 
-   (setq final-cmd
-         (s-join " | " (cons (s-concat (car commands) " " cur-file)
-                     (-drop 1 commands))))
+Column name must be single quoted when there is space in the name."
 
-   (setq buf (get-buffer-create (generate-new-buffer-name "*xsv*")))
-   (set-buffer buf)
-   (setq result (shell-command-to-string final-cmd))
-   (insert result)
-   (select-window (split-window-below))
-   (switch-to-buffer buf)
-))
+  (interactive
+   (list (read-string "select columns(separated by ,): ")))
+
+  (xsv/run columns #'xsv/select-cmd-builder))
 
 (provide 'xsv)
 ;;; xsv.el ends here
